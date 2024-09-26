84d7d10bd3d150e3cff898fd46af1c9557a53d20
Vlad Vasiliev <vlad@example.com>
2024-09-26 17:22:26 +0300

    Using ViewSet classes

--- cats/views.py
+++ cats/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
@@ -8,16 +8,19 @@ from .models import Cat
 from .serializers import CatSerializer
 
 
-class CatList(ListCreateAPIView):
+class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
 
 
-class CatDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Cat.objects.all()
-    serializer_class = CatSerializer
-
-
+# class CatList(ListCreateAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
+#
+#
+# class CatDetail(RetrieveUpdateDestroyAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
 
 
 # class APICat(APIView):
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
 
+from cats.views import CatViewSet
 # from cats.views import APICat, APICatDetail, hello
-from cats.views import CatDetail, CatList
+# from cats.views import CatDetail, CatList
+
+router = SimpleRouter()
+router.register('cats', CatViewSet)
 
 urlpatterns = [
-   path('cats/', CatList.as_view()),
-   path('cats/<int:pk>/', CatDetail.as_view())
+   path('', include(router.urls)),
+   # path('cats/', CatList.as_view()),
+   # path('cats/<int:pk>/', CatDetail.as_view())
    # path('cats/', APICat.as_view()),
    # path('cats/<int:pk>/', APICatDetail.as_view()),
    # path('hello/', hello),
